434c611330c05b668d26e85b863234f3b6775a89
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-12-30 20:57:54 -0800

    removed api_key from App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import { LoadAndProcess } from "./loadAndProcess";
 import React, { useState, useEffect } from "react";
 import { BarChart } from "./components";
-import * as d3 from "d3";
 
 import "./App.css";
 
 function App() {
-  const api_key = "50f2acf0397f4fa3b589ec44fb843786";
-
   const [data, setData] = useState();
 
   useEffect(() => {
