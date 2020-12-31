a74c412545d3ad637a8cb022724775d574dcc31b
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-12-30 21:43:15 -0800

    added a height and width to svg element

--- src/components/barChart.js
+++ src/components/barChart.js
@@ -6,8 +6,8 @@ export default function BarChart({ props }) {
   const svgRef = useRef();
   //   console.log(props);
 
-  const yValue = (d) => d.totalCases;
   const xValue = (d) => d.date;
+  const yValue = (d) => d.totalCases;
 
   useEffect(() => {
     const { worldArr, statesArr } = props;
@@ -16,33 +16,35 @@ export default function BarChart({ props }) {
     console.log(statesArr);
     const svg = d3.select(svgRef.current);
 
-    console.log(svg);
-    const screenWidth = svg.attr("width");
+    const width = +svg.attr("width");
     const height = svg.attr("height");
+
     console.log(worldArr[0].date);
 
     const xScale = d3
       .scaleTime()
-      .domain([new Date(worldArr[0].date), new Date(worldArr[120].date)])
-      .range([0, 300]);
+      .domain(
+        [new Date(worldArr[0].date), new Date(worldArr[120].date)].reverse()
+      )
+      .range([0, width]);
 
     const yScale = d3
-      .scaleTime()
+      .scaleLinear()
       .domain(d3.extent(worldArr, yValue))
-      .range([0, 150]);
+      .range([height, 0]);
 
-    // const xScale = d3
-    //   .scaleTime()
-    //   .domain(
-    //     d3.extent(new Date(worldArr[0].date), new Date(worldArr[120].date))
-    //   );
-    //   .range([0, 300]);
+    const xAxis = d3.axisBottom(xScale).tickPadding(20);
+    const yAxis = d3.axisLeft(yScale);
 
+    svg.select(".x-axis").style("transform", "translateY(100px)").call(xAxis);
+    svg.select(".y-axis").style("transform", "translateX(300px)").call(yAxis);
+
+    svg.select("svg").attr("stroke", "black");
     svg.selectAll(".bar").data(props).join("rect");
   }, [props]);
 
   return (
-    <svg ref={svgRef}>
+    <svg width="960" height="500" ref={svgRef}>
       <g className="y-axis" />
       <g className="x-axis" />
     </svg>
