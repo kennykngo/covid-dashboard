7e9f1b43dac0e05b131a0671636df144ae4953a2
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-12-30 17:30:18 -0800

    got worldArr code working

--- src/loadAndProcess.js
+++ src/loadAndProcess.js
@@ -1,22 +1,13 @@
 import * as d3 from "d3";
-import React, { useEffect, useRef, useState } from "react";
 
 export const formatDate = d3.timeFormat("%x");
 export const formatDay = d3.timeFormat("%j");
 
 const processData = (worldData, statesData) => {
-  // const parseMonth = d3.timeFormat("%m");
   console.log(formatDate(new Date(worldData[0].last_update)));
-
-  const days = d3.range(
-    formatDay(new Date(worldData[worldData.length - 1].last_update)),
-    formatDay(new Date(worldData[0].last_update))
-  );
 
   const dateParse = (d) => formatDate(new Date(d));
 
-  // formatDate(worldData);
-  // const days = d3.range();
   const worldArr = [];
   worldData.forEach((data) => {
     const date = dateParse(data.last_update);
@@ -28,21 +19,10 @@ const processData = (worldData, statesData) => {
       totalCases,
       totalDeaths,
     };
-    // days.forEach((day) => {
-    //   console.log(day);
-    //   // const row = {
-    //   //   data["last_update"]: dateParse
-    //   // },
 
-    //   // data.push(row)
-    // });
     worldArr.push(row);
   });
   console.log(worldArr);
-
-  const globalCases = console.log(
-    new Date(worldData[0].last_update).getMonth()
-  );
 
   const stateCases = statesData.reduce((acc, d) => {
     acc[d["state"]] = d;
@@ -50,7 +30,7 @@ const processData = (worldData, statesData) => {
     return acc;
   }, {});
 
-  return { stateCases, globalCases };
+  return { stateCases, worldArr };
 };
 
 export const LoadAndProcess = () =>
