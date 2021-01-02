6c21a67b0bb7558d3b7020cb386d2bc8d0f958b3
Kenny Ngo <kenny.ngo.design@gmail.com>
2021-01-01 23:26:45 -0800

    reformatted the barChart.js to have variables within function scope, but outside useEffect

--- src/components/barChart.js
+++ src/components/barChart.js
@@ -8,31 +8,19 @@ const Rect = styled.rect`
   fill: red;
   fill-opacity: 0.5;
 `;
+const yValue = (d) => d.totalCases;
 
 // const BarChart = ({ props, forwardedRef, x, y }) => {
 const BarChart = ({ svgWidth, svgHeight, props, x, y }) => {
   console.log(props);
   const svgRef = useRef(null);
-  const width = svgWidth;
-  const height = svgHeight;
-
-  const svg = d3.select(svgRef.current);
   const { worldArr, statesArr } = props;
   const globalArr = worldArr.slice(0, 90).reverse();
 
-  console.log(svg);
+  useEffect(() => draw(), [worldArr]);
 
-  const yValue = (d) => d.totalCases;
-  // const xValue = (d) => d.date;
-
-  // const svg = d3.select(svgRef.current).attr("height", 500).attr("width", 960);
-
-  console.log(width, height);
-  // const width = 960;
-  // const height = 500;
-
-  // const width = +svg.attr("width");
-  // const height = +svg.attr("height");
+  const width = svgWidth;
+  const height = svgHeight;
 
   const margin = { top: 60, right: 80, bottom: 88, left: 150 };
   const innerWidth = width - margin.left - margin.right;
@@ -51,99 +39,91 @@ const BarChart = ({ svgWidth, svgHeight, props, x, y }) => {
     .domain(d3.extent(globalArr, yValue))
     .range([innerHeight, 0]);
 
-  // const svg = d3.select(svgRef.current).attr("height", 500).attr("width", 960);
+  const draw = () => {
+    const svg = d3.select(svgRef.current);
 
-  // const width = +svg.attr("width");
-  // const height = +svg.attr("height");
+    console.log(svg);
 
-  // const margin = { top: 60, right: 80, bottom: 88, left: 150 };
-  // const innerWidth = width - margin.left - margin.right;
-  // const innerHeight = height - margin.top - margin.bottom;
+    // const xValue = (d) => d.date;
 
-  // // const xValue = (d) => d.date;
-  // const yValue = (d) => d.totalCases;
+    const xScale = d3
+      .scaleTime()
+      .domain([
+        new Date(globalArr[0].date),
+        new Date(globalArr[globalArr.length - 1].date),
+      ])
+      .range([0, innerWidth]);
 
-  // const xScale = d3
-  //   .scaleTime()
-  //   .domain([
-  //     new Date(globalArr[0].date),
-  //     new Date(globalArr[globalArr.length - 1].date),
-  //   ])
-  //   .range([0, innerWidth]);
+    const yScale = d3
+      .scaleLinear()
+      .domain(d3.extent(globalArr, yValue))
+      .range([innerHeight, 0]);
 
-  // const yScale = d3
-  //   .scaleLinear()
-  //   .domain(d3.extent(globalArr, yValue))
-  //   .range([innerHeight, 0]);
+    const xAxis = d3.axisBottom(xScale);
+    const yAxis = d3.axisRight(yScale);
 
-  // useEffect(() => {
-  //   console.log(globalArr);
-  //   console.log(statesArr);
+    const g = svg.selectAll(".container").data([null]);
+    const gEnter = g.enter().append("g").attr("class", "container");
+    gEnter
+      .merge(g)
+      .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-  //   const xAxis = d3.axisBottom(xScale);
-  //   const yAxis = d3.axisRight(yScale);
+    const xAxisG = g.select(".x-axis");
+    const xAxisGEnter = gEnter.append("g").attr("class", "x-axis");
+    xAxisGEnter.merge(xAxisG).call(xAxis).selectAll(".domain").remove();
 
-  //   const g = svg.selectAll(".container").data([null]);
-  //   const gEnter = g.enter().append("g").attr("class", "container");
-  //   gEnter
-  //     .merge(g)
-  //     .attr("transform", `translate(${margin.left}, ${margin.top})`);
+    const yAxisG = g.select(".y-axis");
+    const yAxisGEnter = gEnter.append("g").attr("class", "y-axis");
+    yAxisGEnter.merge(yAxisG).call(yAxis).selectAll(".domain").remove();
 
-  //   const xAxisG = g.select(".x-axis");
-  //   const xAxisGEnter = gEnter.append("g").attr("class", "x-axis");
-  //   xAxisGEnter.merge(xAxisG).call(xAxis).selectAll(".domain").remove();
+    xAxisGEnter
+      .append("text")
+      .attr("class", "axis-label")
+      .attr("x", -93)
+      .attr("transform", `rotate(90)`)
+      .attr("fill", "black")
+      .attr("text-anchor", "middle")
+      .merge(xAxisG.select(".axis-label"))
+      .attr("y", -innerWidth / 2);
 
-  //   const yAxisG = g.select(".y-axis");
-  //   const yAxisGEnter = gEnter.append("g").attr("class", "y-axis");
-  //   yAxisGEnter.merge(yAxisG).call(yAxis).selectAll(".domain").remove();
+    yAxisGEnter
+      .append("text")
+      .attr("class", "axis-label")
+      .attr("y", -93)
+      .attr("transform", `rotate(90)`)
+      .attr("fill", "black")
+      .attr("text-anchor", "middle")
+      .merge(yAxisG.select(".axis-label"))
+      .attr("x", -innerHeight / 2);
 
-  //   xAxisGEnter
-  //     .append("text")
-  //     .attr("class", "axis-label")
-  //     .attr("x", -93)
-  //     .attr("transform", `rotate(90)`)
-  //     .attr("fill", "black")
-  //     .attr("text-anchor", "middle")
-  //     .merge(xAxisG.select(".axis-label"))
-  //     .attr("y", -innerWidth / 2);
+    gEnter
+      .selectAll("rect")
+      .data(globalArr)
+      .join("rect")
+      .attr("height", (d) => yValue(d) / innerHeight)
+      .attr("x", (d, i) => (i * innerWidth) / globalArr.length)
+      .attr("y", (d) => yScale(yValue(d)))
+      .attr("width", (d, i) => innerWidth / globalArr.length)
+      .attr("fill", "black")
+      .on("mouseenter", function (e, value) {
+        const index = svg.selectAll("rect").nodes().indexOf(this);
 
-  //   yAxisGEnter
-  //     .append("text")
-  //     .attr("class", "axis-label")
-  //     .attr("y", -93)
-  //     .attr("transform", `rotate(90)`)
-  //     .attr("fill", "black")
-  //     .attr("text-anchor", "middle")
-  //     .merge(yAxisG.select(".axis-label"))
-  //     .attr("x", -innerHeight / 2);
+        gEnter
+          .selectAll(".tooltip")
+          .data([value])
+          .join((enter) => enter.append("text").attr("y", yScale(value)))
+          .text(`${value.date} \n ${value.totalCases}`)
+          .attr("class", "tooltip")
+          .attr("x", () => (index * innerWidth) / globalArr.length)
+          .attr("y", yScale(yValue(value)) - 12)
+          .attr("text-anchor", "middle")
+          .attr("opacity", 0.5)
+          .style("background-color", "red");
+        console.log(value, index);
+      });
+    // .on("mouseleave", () => svg.select(".tooltip").remove());
+  };
 
-  //   gEnter
-  //     .selectAll("rect")
-  //     .data(globalArr)
-  //     .join("rect")
-  //     .attr("height", (d) => yValue(d) / innerHeight)
-  //     .attr("x", (d, i) => (i * innerWidth) / globalArr.length)
-  //     .attr("y", (d) => yScale(yValue(d)))
-  //     .attr("width", (d, i) => innerWidth / globalArr.length)
-  //     .attr("fill", "black")
-  //     .on("mouseenter", function (e, value) {
-  //       const index = svg.selectAll("rect").nodes().indexOf(this);
-
-  //       gEnter
-  //         .selectAll(".tooltip")
-  //         .data([value])
-  //         .join((enter) => enter.append("text").attr("y", yScale(value)))
-  //         .text(`${value.date} \n ${value.totalCases}`)
-  //         .attr("class", "tooltip")
-  //         .attr("x", () => (index * innerWidth) / globalArr.length)
-  //         .attr("y", yScale(yValue(value)) - 12)
-  //         .attr("text-anchor", "middle")
-  //         .attr("opacity", 0.5)
-  //         .style("background-color", "red");
-  //       console.log(value, index);
-  //     });
-  //   // .on("mouseleave", () => svg.select(".tooltip").remove());
-  // }, [props]);
   const bars = globalArr.map((d, i) => (
     <Rect
       height={yValue(d) / innerHeight}
@@ -154,7 +134,7 @@ const BarChart = ({ svgWidth, svgHeight, props, x, y }) => {
   ));
 
   return (
-    <svg ref={svgRef} height={height} width={width}>
+    <svg ref={svgRef} height={svgHeight} width={svgWidth}>
       <g transform={`translate(${x}, ${y})`}>{bars}</g>
       <g className="y-axis" />
       <g className="x-axis" />
