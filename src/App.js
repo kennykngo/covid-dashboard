8cd364b0492323be31be424b5b5a479c9fc272e2
Kenny Ngo <kenny.ngo.design@gmail.com>
2021-01-05 14:17:06 -0800

    imported react-use dependency for useMeasure

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from "react";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
+import { useMeasure } from "react-use";
 
 import { BarChart, RadioButtonsGroup } from "./components";
 import { LoadAndProcess } from "./loadAndProcess";
@@ -11,6 +12,7 @@ function App() {
   var [selectedBar, setSelectedBar] = useState();
   let [currentCase, setCurrentCase] = useState("total cases");
   const globalCaseArr = ["total cases", "total deaths"];
+  const [ref, { x, y, width, height, top, right, bottom, left }] = useMeasure();
 
   const onMouseOver = (d) => {
     return setSelectedBar((selectedBar = d));
@@ -36,21 +38,33 @@ function App() {
       />
       <Container>
         <Row>
-          {data ? (
-            <BarChart
-              currentCase={currentCase}
-              svgWidth={960}
-              svgHeight={500}
-              // selectedRectBar={selectedBar}
-              margin={margin}
-              // onMouse={onMouseOver}
-              props={data}
-              x={margin.left}
-              y={margin.top}
-            />
-          ) : (
-            <h1>Loading...</h1>
-          )}
+          <Col sm={12} lg={6}>
+            {data ? (
+              <BarChart
+                currentCase={currentCase}
+                svgWidth={960}
+                svgHeight={500}
+                // selectedRectBar={selectedBar}
+                margin={margin}
+                // onMouse={onMouseOver}
+                props={data}
+                x={margin.left}
+                y={margin.top}
+              />
+            ) : (
+              <h1>Loading...</h1>
+            )}
+          </Col>
+          <Col xs={12} lg={6} ref={ref}>
+            <div>x: {x}</div>
+            <div>y: {y}</div>
+            <div>width: {width}</div>
+            <div>height: {height}</div>
+            <div>top: {top}</div>
+            <div>right: {right}</div>
+            <div>bottom: {bottom}</div>
+            <div>left: {left}</div>
+          </Col>
         </Row>
       </Container>
     </div>
