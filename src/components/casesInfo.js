0451ca856dcc9ad84b8204bf70b614fa84909a47
Kenny Ngo <kenny.ngo.design@gmail.com>
2021-01-10 11:34:21 -0800

    created a footnote style for covid casesInfo

--- src/components/casesInfo.js
+++ src/components/casesInfo.js
@@ -1,15 +1,23 @@
 import React from "react";
 import Col from "react-bootstrap/Col";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Row from "react-bootstrap/Row";
+import "./__casesStyles.scss";
 
 const CasesInfo = ({ title, addition }) => {
   return (
-    <Col className="d-flex">
-      <h6> {title}</h6>
+    <Col>
+      <Row className="d-flex justify-content-center">
+        <div className="p-3 d-flex justify-content-center flex-column">
+          <p className="text-center"> {title}</p>
 
-      <div className="p-3">
-        <p>{addition > 0 ? "+" + addition : addition}</p>
-      </div>
+          <div className="text-center">
+            <div className="bg-dark text-white d-inline py-1 px-2 rounded c-footnote">
+              {addition > 0 ? "+" + addition : addition}
+            </div>
+          </div>
+        </div>
+      </Row>
     </Col>
   );
 };
