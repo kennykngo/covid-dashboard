d4ad5363c9cf24281e086389f574c31f822382eb
Kenny Ngo <kenny.ngo.design@gmail.com>
2021-01-10 22:13:57 -0800

    refactored the code to map out the components

--- src/components/casesModule.js
+++ src/components/casesModule.js
@@ -10,28 +10,37 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import styled from "styled-components";
 
 export default function CasesModule({ title, data, date }) {
+  const casesInfoArr = [
+    {
+      covidTitle: "Confirmed",
+      addition: data[0].totalCases - data[1].totalCases,
+      cases: data[0].totalCases,
+    },
+    {
+      covidTitle: "Deaths",
+      addition: data[0].totalDeaths - data[1].totalDeaths,
+      cases: data[0].totalDeaths,
+    },
+    {
+      covidTitle: "Recovered",
+      addition: data[0].totalRecovered - data[1].totalRecovered,
+      cases: data[0].totalRecovered,
+    },
+  ];
+
   return (
     <Row className="cases-module-style pt-3 px-2 pb-3 bg-white">
       <div className="mb-2">
         <div>{title}</div>
         <div className="c-date"> Last Updated: {date}</div>
       </div>
-
-      <CasesInfo
-        title="Confirmed"
-        addition={data[0].totalCases - data[1].totalCases}
-        cases={data[0].totalCases}
-      />
-      <CasesInfo
-        title="Deaths"
-        addition={data[0].totalDeaths - data[1].totalDeaths}
-        cases={data[0].totalDeaths}
-      />
-      <CasesInfo
-        title="Recovered"
-        addition={data[0].totalRecovered - data[1].totalRecovered}
-        cases={data[0].totalRecovered}
-      />
+      {casesInfoArr.map((data) => (
+        <CasesInfo
+          title={data.covidTitle}
+          addition={data.addition}
+          cases={data.cases}
+        />
+      ))}
     </Row>
   );
 }
