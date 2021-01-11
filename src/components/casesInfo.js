4a68e9f2e76bad09d3aced9bbddf81ee9c194898
Kenny Ngo <kenny.ngo.design@gmail.com>
2021-01-10 17:17:54 -0800

    added an nth-child psuedo selector for the cases-info-row

--- src/components/casesInfo.js
+++ src/components/casesInfo.js
@@ -3,14 +3,23 @@ import Col from "react-bootstrap/Col";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Row from "react-bootstrap/Row";
 import "./__casesStyles.scss";
+import styled from "styled-components";
+
+const P = styled.p`
+  text-align: center;
+  display: inline-block;
+  margin-bottom: 0;
+  color: #666;
+  font-size: 13px;
+`;
 
 const CasesInfo = ({ title, addition }) => {
   return (
-    <Col>
+    <Col className="cases-info-row">
       <Row className="d-flex justify-content-center">
         <div className="p-3 d-flex justify-content-center flex-column">
-          <p className="text-center"> {title}</p>
-
+          <P> {title}</P>
+          <div className="d-block text-center">26,000</div>
           <div className="text-center">
             <div className="bg-dark text-white d-inline py-1 px-2 rounded c-footnote">
               {addition > 0 ? "+" + addition : addition}
--- src/components/casesModule.js
+++ src/components/casesModule.js
@@ -3,6 +3,15 @@ import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 
 import CasesInfo from "./casesInfo";
+import "./__casesStyles.scss";
+
+import styled from "styled-components";
+
+// const Span = styled.span`
+//   border-left: 1px solid black;
+//   display: inline-block;
+//   height: 184px;
+// `;
 
 export default function CasesModule({ data }) {
   console.log(data);
